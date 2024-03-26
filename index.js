5644a9a3388abbe287cf0eb1e3c13743135152dd
ara fa adri <arafa.sco@gmail.com>
2024-03-26 08:03:11 +0700

    open index

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ app.use(session({
     saveUninitialized: true,
     store:store,
     cookie: {
-        httpOnly: true,
-        // secure: 'auto',
+        // httpOnly: true,
+        secure: 'auto',
         maxAge: 1000 * 60 * 60
     }
 }));
@@ -109,24 +109,6 @@ app.use(ResetPassword);
 
 //setup public folder
 app.use(express.static("public"));
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', `${process.env.URL_ORIGIN}`);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 // store.sync();
 
