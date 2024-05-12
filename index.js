772f38f9c680d15ede87714dfb4d495279e59904
ara fa adri <arafa.sco@gmail.com>
2024-05-12 22:31:32 +0700

    open sync

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ const store = new sessionStore({
     db:db
 });
 
-// (async()=>{
-//     await db.sync();
-// })();
+(async()=>{
+    await db.sync();
+})();
 
 app.use(session({
     secret: process.env.SESS_SECRET,
