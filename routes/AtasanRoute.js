222497a95c1ed655ba057d3f36dedd09a15c4171
ara fa adri <arafa.sco@gmail.com>
2024-05-27 17:35:23 +0700

    delete isi import file

--- routes/AtasanRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAtasans } from '../controllers/Atasan.js';
-
-const router = express.Router();
-
-router.get('/atasans', getAtasans);
-
-export default router;
